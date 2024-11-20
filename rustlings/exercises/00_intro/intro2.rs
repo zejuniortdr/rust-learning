79782af2612cf78c8febbdba910db6f31d2da082
Jose Luis da Cruz Junior <zejuniortdr@gmail.com>
2024-11-20 05:59:24 -0300

    rustlings exercices 00_intro DONE

---
+++ rustlings/exercises/00_intro/intro2.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // DONE: Fix the code to print "Hello world!".
+
+    // printline!("Hello world!");
+    // ^^^^^^^^^ help: a macro with a similar name exists: `println`
+
+    println!("Hello world!");
+}
